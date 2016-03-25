609f38739b8dcf85b77c152d3896bdefeaa3dba8
Brad Pizzimenti <brad@gennakersystems.com>
2016-03-25 12:57:05 -0700

    empty repoName div prior to for-loop for appending info (fix old repos not disappearing after submitting another user)

--- js/ghQuery.js
+++ js/ghQuery.js
@@ -4,15 +4,16 @@ exports.getRepos = function(username) {
   $.get('https://api.github.com/users/'+username+'?access_token=' + apiKey).then(function(response){
     console.log(response);
     $('#userImg').html("<img src="+response.avatar_url+"</img>");
-    $('#userName').html("<h3>"+response.login+"</h3>");
+    $('#userName').html("<h3><a href='https://github.com/"+response.login+"'>"+response.login+"</h3>");
     $('#realName').html("<h4>"+response.name+"</h4>");
   }).fail(function(error){
       console.log(error.responseJSON.message);
     });
 
+$('#repoName').empty();
   $.get('https://api.github.com/users/'+username+'/repos?access_token=' + apiKey).then(function(repo){
     for(var i=0; i<repo.length; i++){
-      $('#repoName').append("<h5>"+repo[i].name+"</h5>");
+      $('#repoName').append("<h5><a href='http://github.com/"+username+"/"+repo[i].name+"'>"+repo[i].name+"</a></h5>");
       if(repo[i].description != null) {
         $('#repoName').append("<p>"+repo[i].description+'</p><br>');
       }
