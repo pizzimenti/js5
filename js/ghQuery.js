c60502510119723ef184b9aca7e7dc0abd6fd7a0
Brad Pizzimenti <brad@gennakersystems.com>
2016-03-25 12:32:54 -0700

    display name and description of first 30 repos

--- js/ghQuery.js
+++ js/ghQuery.js
@@ -3,15 +3,21 @@ var apiKey = require('./../.env').apiKey;
 exports.getRepos = function(username) {
   $.get('https://api.github.com/users/'+username+'?access_token=' + apiKey).then(function(response){
     console.log(response);
-    console.log(response.avatar_url);
-    console.log(response.login);
-    console.log(response.name);
-    console.log('followers :',response.followers);
-    console.log(response.repos_url);
     $('#userImg').html("<img src="+response.avatar_url+"</img>");
     $('#userName').html("<h3>"+response.login+"</h3>");
     $('#realName').html("<h4>"+response.name+"</h4>");
   }).fail(function(error){
       console.log(error.responseJSON.message);
     });
-  };
+
+  $.get('https://api.github.com/users/'+username+'/repos?access_token=' + apiKey).then(function(repo){
+    for(var i=0; i<repo.length; i++){
+      $('#repoName').append("<h5>"+repo[i].name+"</h5>");
+      if(repo[i].description != null) {
+        $('#repoName').append("<p>"+repo[i].description+'</p><br>');
+      }
+    }
+  }).fail(function(error){
+      console.log(error.responseJSON.message);
+    });
+};
